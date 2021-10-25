561de47e322529bcce7fd21c040e50d345ea78dd
IdunilSampath <84393300+IdunilSampath@users.noreply.github.com>
2021-10-26 00:45:45 +0530

    Create Idunil.py
    git clone https://github.com/IdunilSampath/Idunil.py.git
    cd Idunil.py
    python2 Idunil.py

---
+++ Idunil.py
@@ -0,0 +1,61 @@
+#coding=utf-8
+#!/usr/bin/python2
+#coding=utf-8
+try:
+    import os,re,time
+except IOError:
+    os.system("pip2 install requests")
+    os.system("pip2 install mechanize")
+    os.system("python2 infect.xo")
+
+try:
+    os.mkdir("/sdcard/infect-tool")
+except OSError:
+    pass
+
+abm = """
+\033[1;97m  #     #           # # 
+\033[1;97m  #     #             # 
+\033[1;97m  #  #### #  # #### # # 
+\033[1;97m  # #   # #  # #  # # # 
+\033[1;97m  # #   # #  # #  # # # 
+\033[1;97m  #  #### #### #  # # # 
+\033[1;97m------------------------------------------------- 
+\033[1;97m(~) Author  : 𝐈𝐝𝐮𝐧𝐢𝐥 𝐒𝐚𝐦𝐩𝐚𝐭𝐡
+\033[1;97m(~) Github  : https://github.com/IdunilSampath
+\033[1;97m-------------------------------------------------
+"""
+
+def main():
+    os.system("clear")
+    print(abm)
+    os.system("cd load && python2 __loading__")
+    os.system("clear")
+    print(abm)
+    print("\033[1;97m[\033[1;93m1\033[1;97m] Install infect tool for cloning")
+    time.sleep(0.05)
+    print("\033[1;97m[\033[1;93m2\033[1;97m] Install infect extracting tool")
+    time.sleep(0.05)
+    print("\033[1;97m[\033[1;93m0\033[1;97m] Tool Logout")
+    time.sleep(0.05)
+    print("\033[1;97m-------------------------------------------------")
+    mx()
+def mx():
+    tech_abm = raw_input("\n[!] Select a valid option : ")
+    if tech_abm =="1":
+        os.system("cd data && python2 data")
+    if tech_abm =="2":
+        os.system("cd exts && python2 exts")
+    if tech_abm =="0":
+        print("")
+        print("\033[1;92mTool Logout Successfull").center(50)
+        print("")
+        os.system("exit")
+    else:
+        print("")
+        print("\033[1;91mPlease select a valid option").center(50)
+        print("")
+        time.sleep(1)
+        main()
+if __name__ == '__main__':
+    main()
